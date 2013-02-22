4f28d12e1fd7b58367fd7a0785eb4ac59044f4cc
Jesper Joergensen <jesper@heroku.com>
2013-02-22 09:50:16 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unicorn1::Application.config.secret_token = '63947c8d07f44a5a3c1554bedc87f1d34619a198a4d1f4e9bf4adc38230654184d00128e54f5d276fa1927fe4520bd30dcb205cacdfc54211e2d7049c8ab807c'
