711b29b773d18c2584420b3ad3486d3012279cdb
Jesper Joergensen <jesper@heroku.com>
2013-03-02 18:13:54 -0800

    raindrop

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+
+  def index
+  	require 'socket'
+	ip = IPSocket.getaddress(Socket.gethostname)
+	puts ip.to_s + ':' + ENV['PORT']
+  	stats = Raindrops::Linux.tcp_listener_stats([ ip.to_s + ':' + ENV['PORT'] ])
+  	stats.each { |addr, stats|
+  		printf fmt, addr, stats.active, stats.queued
+  	}
+  end
+end
