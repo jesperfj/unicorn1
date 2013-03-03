2a3f1c6c6824c762de9de37f20e31341350d1041
Jesper Joergensen <jesper@heroku.com>
2013-03-02 20:17:32 -0800

    hacking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,45 @@
+require 'socket'
+require 'random_gaussian'
+
 class HomeController < ApplicationController
 
+  before_filter :before
+  after_filter :after
+
   def index
-  	require 'socket'
-	ip = IPSocket.getaddress(Socket.gethostname)
-	puts ip.to_s + ':' + ENV['PORT']
-  	stats = Raindrops::Linux.tcp_listener_stats([ ip.to_s + ':' + ENV['PORT'] ])
-  	stats.each { |addr, stats|
-  		puts "#{addr}, #{stats.active}, #{stats.queued}"
-  	}
   end
+
+  protected
+
+    ADDR = IPSocket.getaddress(Socket.gethostname).to_s+':'+ENV['PORT']
+
+    WAIT = RandomGaussian.new(50,20)
+
+	  def tcp_stats
+      if defined? Raindrops::Linux.tcp_listener_stats
+        stats = Raindrops::Linux.tcp_listener_stats([ ADDR ])[ADDR]
+        return { :active => stats.active, :queued => stats.queued }	  	
+      else
+        return { :active => 0, :queued => 0 }     
+		  end
+	  end
+
+	  def before
+	  	@stats = tcp_stats
+	  	@request_start = Time.now.to_i
+      if request.headers['X-Request-Start']
+        puts request.headers['X-Request-Start']
+        @queue_time = request.headers['X-Request-Start'].to_i-@request_start
+      else
+        @queue_time = 0
+      end
+	  end
+
+	  def after
+      request_time = Time.now.to_i - @request_start
+      puts "tcp_addr=\"#{ADDR}\" tcp_active=#{@stats[:active]} tcp_queued=#{@stats[:queued]} queue_time=#{@queue_time} request_time=#{request_time}"
+	  end
+
 end
+
+
