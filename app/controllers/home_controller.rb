70e3f9512f7ac5332647d46cd666d943fca7f419
Jesper Joergensen <jesper@heroku.com>
2013-03-12 15:16:19 -0700

    added long tail

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,13 +11,19 @@ class HomeController < ApplicationController
   WAIT_SDEV = ENV['WAIT_SDEV'].to_f || 0.02
 
   def index
-    sleep [WAIT.rand,0].max
+    if rand <= 0.1
+      sleep [LONG_WAIT,0].max
+    else
+      sleep [WAIT.rand,0].max
+    end
     render :nothing => true, :status => :ok
   end
 
   protected
 
     WAIT = RandomGaussian.new(WAIT_MEAN,WAIT_SDEV)
+
+    LONG_WAIT = RandomGaussian.new(WAIT_MEAN*10,WAIT_SDEV*10)
 
 end
 
